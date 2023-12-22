805f144b9139002b7f14588d885bef67590e8578
Taynara Araújo <112909056+Thay483@users.noreply.github.com>
2023-12-22 18:08:04 -0300

    Força resultante (Soma) - Funcionando

--- fisica_calc.h
+++ fisica_calc.h
@@ -13,22 +13,67 @@
 #define ELETROSTATICA 9.0e-9
 #define MOD_CARGA 1.602e-19
 
+//ESTRUTURA
+//Lista de forças
+struct forcas
+{
+    float forca;
+    struct forcas *prox;
+};
+typedef struct forcas Forcas;
+
+struct lista
+{
+    Forcas *inicio;
+};
+typedef struct lista Lista;
+
+
 //PROTÓTIPOS
+Lista *InicializaListaForca();
+Lista *InsereForcaLista(Lista *l, float valor);
+void DesalocarListaForca(Lista *l);
 void Inicializador();
 void MenuFisica();
 int VerificarMod();
 int VerificaCarga();
-// float *VetCarga();
-// float *VetDistancia();
-void ModForcaEletrostatica();
-// float VetForcaEletrostatica();
+//void ModForcaEletrostatica();
 int RespostaCalc();
 float CalcForca(float q1, float q2, float d);
 float CalcDistancia(float f, float q1, float q2);
 float CalcCarga(float f, float q, float d, int resp);
+Lista *CalForcaResultante(Lista *l, float q1, float q2, float d);
+float SomaListaForca(Forcas *vet);
+void ImprimirListaForca(Forcas *list);
 //int verificar_resp(int resp);
 
 // IMPLEMENTAÇÕES
+// // Inializa a lista como nula
+Lista *InicializaListaForca(){
+    Lista *l = (Lista *)malloc(sizeof(Lista));
+    l->inicio = NULL;
+    return l;
+}
+
+// insere as forças na lista de forças
+Lista *InsereForcaLista(Lista *l, float valor){
+    Forcas *novo = (Forcas *)malloc(sizeof(Forcas));
+    novo->forca = valor;
+    novo->prox = l->inicio;
+    l->inicio = novo;
+    return l;
+}
+// Desaloca lista de forças
+void DesalocarListaForca(Lista *l){
+    Forcas *aux = l->inicio; 
+    while (aux != NULL){
+        Forcas *aux2 = aux->prox;
+        free(aux);
+        aux = aux2;
+    }
+    free(l);
+}
+
 void Inicializador(){
     system("cls");
     printf("CALCULADORA DE FISICA III\n");
@@ -89,14 +134,14 @@ int VerificaCarga(){
     else return 0;
 }
 
-// float *VetCarga(){
-//     int n = VerificaCarga();
-//     float *cargas = (float*)malloc(sizeof(float) * n);
-//     for(int i = 0; i < n; i++){
-//         printf("Digite o valor da carga %d (C): ", i + 1);
-//         scanf("%f", &cargas[i]);
-//     }
-// }
+// função que verifica se o usuário quer calcular a força resultante em uma carga qualquer
+int VerificaForcaResultante(){
+    int resp = 0;
+    system("cls");
+    printf("Quer encontrar forca resultante em uma carga (1-Sim/2-Nao)?\n");
+    if(scanf("%d", &resp) == 1) return 1;
+    else return 0;
+}
 
 // função que calcula a distância de uma carga a algum ponto ou entre duas cargas
 float CalcDistancia(float f, float q1, float q2){
@@ -108,32 +153,45 @@ float CalcDistancia(float f, float q1, float q2){
 
 void ModForcaEletrostatica(){
     float f = 0, q1 = 0, q2 = 0, d = 0;
+    //Forcas *vet = InicializaListaForca();
     int resp = 0;
     resp = RespostaCalc();
     switch (resp){
         case 1:
-            // float *cargas = VetCarga();
-            if(!VerificaCarga()){
-                printf("Digite o valor da carga (C): ");
-                scanf("%f", &q1);
-                printf("Digite o valor da distancia (m): ");
-                scanf("%f", &d);
-                f = CalcForca(q1, q2, d);
-                printf("A forca eletrostatica e: %.10f N\n", f);
+            if(VerificaForcaResultante() != 1){
+                if(!VerificaCarga()){
+                    system("cls");
+                    printf("Digite o valor da carga (C): ");
+                    scanf("%f", &q1);
+                    printf("Digite o valor da distancia (m): ");
+                    scanf("%f", &d);
+                    f = CalcForca(q1, q2, d);
+                    printf("A forca eletrostatica e: %.10f N\n", f);
+                    system("pause");
+                }
+                else{
+                    system("cls");
+                    printf("Digite o valor da carga 1 (C): ");
+                    scanf("%f", &q1);
+                    printf("Digite o valor da carga 2 (C): ");
+                    scanf("%f", &q2);
+                    printf("Digite o valor da distancia (m): ");
+                    scanf("%f", &d);
+                    f = CalcForca(q1, q2, d);
+                    printf("A forca eletrostatica e: %.10f N\n", f);
+                    system("pause");
+                }
+            }else{
+                Lista *l = InicializaListaForca();
+                l = CalForcaResultante(l, q1, q2, d);
+                ImprimirListaForca(l->inicio);
+                f = SomaListaForca(l->inicio);
+
+                printf("A forca eletrostatica resultante e: %.10f N\n", f);
                 system("pause");
+                DesalocarListaForca(l);
             }
-            else{
-                printf("Digite o valor da carga 1 (C): ");
-                scanf("%f", &q1);
-                printf("Digite o valor da carga 2 (C): ");
-                scanf("%f", &q2);
-                printf("Digite o valor da distancia (m): ");
-                scanf("%f", &d);
-                f = CalcForca(q1, q2, d);
-                printf("A forca eletrostatica e: %.10f N\n", f);
-                system("pause");
-            }
-            break;
+                break;
         case 2:
             printf("Digite o valor da forca (N): ");
             scanf("%f", &f);
@@ -178,10 +236,57 @@ void ModForcaEletrostatica(){
             }
             break;
         case 5:
+            //DesalocarListaForca(vet);
             break;
         default:
             printf("Opcao invalida!\n");
             break;
+    }
+}
+
+// soma o modulo das forças e retorna o valor da força resultante
+float SomaListaForca(Forcas *vet){
+    float soma = 0;
+    Forcas *aux = vet;
+    while(aux != NULL){
+        soma += aux->forca;
+        aux = aux->prox;
+    }
+    return soma;
+}
+
+// função que cria uma lista de forças enquanto o usuário desejar
+// o cálculo é feito com base no cálcula da força eletrostática entre duas cargas
+Lista *CalForcaResultante(Lista *l, float q1, float q2, float d){
+    Lista *aux = l;
+    float f = 0;
+    int resp = 0;
+    system("cls");
+    printf("Calculando a Forca Resultante em uma carga\n");
+    system("pause");   
+    do{
+        printf("Digite o valor da carga 1 (C): ");
+        scanf("%f", &q1);
+        printf("Digite o valor da carga 2 (C): ");
+        scanf("%f", &q2);
+        printf("Digite o valor da distancia (m): ");
+        scanf("%f", &d);
+        f = CalcForca(q1, q2, d);
+        printf("A forca eletrostatica e: %.10f N\n", f);
+        
+        aux = InsereForcaLista(aux, f);
+        printf("Deseja parar? (1-Sim/2-Nao)\n");
+        scanf("%d", &resp);
+
+    }while(resp != 1);
+    return aux;
+}
+
+void ImprimirListaForca(Forcas *list){
+    Forcas *aux = list;
+    while(aux != NULL){
+        printf("Forca: %.10f N\n", aux->forca);
+        aux = aux->prox;
     }
 }
 
